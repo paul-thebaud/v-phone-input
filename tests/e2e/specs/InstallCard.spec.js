c3f6133980d999f5519ce663e3614d8afa43a546
Paul Thebaud <paul.thebaud29@gmail.com>
2022-03-02 15:20:36 +0100

    test: add install card tests

---
+++ tests/e2e/specs/InstallCard.spec.js
@@ -0,0 +1,52 @@
+describe('InstallCard.vue', () => {
+  it('should display title and description', () => {
+    cy.visitDemo('FR');
+
+    cy.dataCy('install-card')
+      .contains('h2', 'Installation');
+    cy.dataCy('install-card')
+      .contains('You can install the package through Yarn or NPM.');
+  });
+
+  it('should copy yarn command and show alert', () => {
+    cy.visitDemo('FR');
+
+    cy.dataCy('install-card')
+      .contains('Yarn add copied to clipboard.')
+      .should('not.exist');
+
+    cy.dataCy('install-card')
+      .get('button[title="Copy Yarn add to clipboard"]')
+      .click();
+
+    cy.dataCy('install-card')
+      .contains('Yarn add copied to clipboard.');
+
+    cy.window().then((win) => {
+      win.navigator.clipboard.readText().then((text) => {
+        assert.equal(text, 'yarn add v-phone-input');
+      });
+    });
+  });
+
+  it('should copy npm command and show alert', () => {
+    cy.visitDemo('FR');
+
+    cy.dataCy('install-card')
+      .contains('NPM install copied to clipboard.')
+      .should('not.exist');
+
+    cy.dataCy('install-card')
+      .get('button[title="Copy NPM install to clipboard"]')
+      .click();
+
+    cy.dataCy('install-card')
+      .contains('NPM install copied to clipboard.');
+
+    cy.window().then((win) => {
+      win.navigator.clipboard.readText().then((text) => {
+        assert.equal(text, 'npm install v-phone-input');
+      });
+    });
+  });
+});
