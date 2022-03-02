fc76d570237bf26d6876323dfa14602617a0e52d
Paul Thebaud <paul.thebaud29@gmail.com>
2022-03-02 14:41:42 +0100

    refactor: app test

--- tests/e2e/specs/App.spec.js
+++ tests/e2e/specs/App.spec.js
@@ -1,5 +1,5 @@
 describe('App.vue', () => {
-  it('should display demo titles', () => {
+  it('should display all titles', () => {
     cy.visitDemo('FR');
     cy.contains('h1', 'VPhoneInput');
     cy.contains('h2', 'Installation');
@@ -9,16 +9,12 @@ describe('App.vue', () => {
 
   it('should match snapshot', () => {
     cy.visitDemo('FR');
-    cy.contains('h1', 'VPhoneInput');
-    cy.contains('h2', 'Installation');
-    cy.contains('h2', 'Configuration');
-    cy.contains('h2', 'Input preview');
-
     cy.get('#app').toMatchImageSnapshot();
   });
 
   it('should toggle theme to dark and match snapshot', () => {
     cy.visitDemo('FR');
+
     cy.get('[data-cy=dark-switch]').parent().click();
 
     cy.get('#app').toMatchImageSnapshot();
