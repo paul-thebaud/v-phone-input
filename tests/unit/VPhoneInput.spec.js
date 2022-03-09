1c34c32c30f7e327cd4d3e5b7c3531dbda5728c6
Paul Thebaud <paul.thebaud29@gmail.com>
2022-03-09 15:06:28 +0100

    test: add unit tests for input

--- tests/unit/VPhoneInput.spec.js
+++ tests/unit/VPhoneInput.spec.js
@@ -12,9 +12,10 @@ describe('VPhoneInput.vue', () => {
   it('should use all countries', () => {
     const wrapper = makeVPhoneInput();
 
-    expect(wrapper.vm.sortedCountries.length).toEqual(250);
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'AF')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.length).toEqual(250);
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'AF')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.divider === true)).toBeFalsy();
   });
 
   it('should filter countries using onlyCountries', () => {
@@ -22,9 +23,10 @@ describe('VPhoneInput.vue', () => {
       propsData: { onlyCountries: ['FR', 'be'] },
     });
 
-    expect(wrapper.vm.sortedCountries.length).toEqual(2);
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'AF')).toBeFalsy();
+    expect(wrapper.vm.countriesItems.length).toEqual(2);
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'AF')).toBeFalsy();
+    expect(wrapper.vm.countriesItems.find((c) => c.divider === true)).toBeFalsy();
   });
 
   it('should filter countries using ignoredCountries', () => {
@@ -32,8 +34,31 @@ describe('VPhoneInput.vue', () => {
       propsData: { ignoredCountries: ['FR', 'be'] },
     });
 
-    expect(wrapper.vm.sortedCountries.length).toEqual(248);
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeFalsy();
-    expect(wrapper.vm.sortedCountries.find((c) => c.iso2 === 'AF')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.length).toEqual(248);
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeFalsy();
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'AF')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.divider === true)).toBeFalsy();
+  });
+
+  it('should prepend preferred countries with divider', () => {
+    const wrapper = makeVPhoneInput({
+      propsData: { preferredCountries: ['FR', 'be'] },
+    });
+
+    expect(wrapper.vm.countriesItems.length).toEqual(251);
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'AF')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.divider === true)).toBeTruthy();
+  });
+
+  it('should prepend preferred countries without divider', () => {
+    const wrapper = makeVPhoneInput({
+      propsData: { preferredCountries: ['FR', 'be'], onlyCountries: ['FR', 'BE'] },
+    });
+
+    expect(wrapper.vm.countriesItems.length).toEqual(2);
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'FR' || c.iso2 === 'BE')).toBeTruthy();
+    expect(wrapper.vm.countriesItems.find((c) => c.iso2 === 'AF')).toBeFalsy();
+    expect(wrapper.vm.countriesItems.find((c) => c.divider === true)).toBeFalsy();
   });
 });
