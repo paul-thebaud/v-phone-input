a58b3c72f617f07be2fa4942ff3c667740ec3847
Paul Thebaud <paul.thebaud29@gmail.com>
2022-05-24 12:24:03 +0200

    feat: memo country guesser will not memoize when undefined

--- src/utils/countries/memoIp2cCountryGuesser.ts
+++ src/utils/countries/memoIp2cCountryGuesser.ts
@@ -2,13 +2,13 @@ import { CountryIso2 } from '@/types/countries';
 import Ip2cCountryGuesser from '@/utils/countries/ip2cCountryGuesser';
 
 export default class MemoIp2cCountryGuesser extends Ip2cCountryGuesser {
-  private memoGuessPromise = undefined as Promise<CountryIso2 | undefined> | undefined;
+  private memoCountry = undefined as CountryIso2 | undefined;
 
   public async guess(): Promise<CountryIso2 | undefined> {
-    if (!this.memoGuessPromise) {
-      this.memoGuessPromise = super.guess();
+    if (!this.memoCountry) {
+      this.memoCountry = await super.guess();
     }
 
-    return this.memoGuessPromise;
+    return this.memoCountry;
   }
 }
--- tests/unit/memoIp2cCountryGuesser.spec.js
+++ tests/unit/memoIp2cCountryGuesser.spec.js
@@ -1,12 +1,22 @@
 import { MemoIp2cCountryGuesser } from '@/entry.esm';
 
 describe('memoIp2cCountryGuesser.js', () => {
-  it('should memoize promise from ip2 country guesser', async () => {
+  it('should memoize promise from ip2 country guesser when not undefined', async () => {
     global.fetch = jest.fn(() => Promise.resolve({
-      text: () => Promise.resolve('1;FR'),
+      text: () => Promise.resolve('0'),
     }));
 
     const memoIp2cCountryGuesser = new MemoIp2cCountryGuesser();
+
+    expect(await memoIp2cCountryGuesser.guess()).toBeUndefined();
+    expect(global.fetch.mock.calls.length).toBe(1);
+
+    expect(await memoIp2cCountryGuesser.guess()).toBeUndefined();
+    expect(global.fetch.mock.calls.length).toBe(2);
+
+    global.fetch = jest.fn(() => Promise.resolve({
+      text: () => Promise.resolve('1;FR'),
+    }));
 
     expect(await memoIp2cCountryGuesser.guess()).toBe('FR');
     expect(global.fetch.mock.calls.length).toBe(1);
