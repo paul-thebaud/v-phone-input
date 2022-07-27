ac38e2c3aa516f87163ec43291d9ea77dd591b1a
Paul Thebaud <paul.thebaud29@gmail.com>
2022-07-27 18:09:16 +0200

    fix: type for plugin factory options and throw when passed to vue

--- src/createVPhoneInput.ts
+++ src/createVPhoneInput.ts
@@ -3,8 +3,12 @@ import { PluginOptions } from '@/types/options';
 import { mergeOptions } from '@/utils/options';
 import { PluginFunction } from 'vue';
 
-export default function createVPhoneInput(options?: PluginOptions): PluginFunction<PluginOptions> {
-  return (Vue) => {
+export default function createVPhoneInput(options?: Partial<PluginOptions>): PluginFunction<never> {
+  return (Vue, VueOptions) => {
+    if (VueOptions) {
+      throw new Error('options must be passed when calling createVPhoneInput');
+    }
+
     mergeOptions(options || {});
 
     Vue.component('VPhoneInput', VPhoneInput);
