d46b931b1ab4503e84ee61b38bce93d4c1d85cbc
Paul Thébaud <paul.thebaud29@gmail.com>
2023-01-28 20:21:20 +0100

    ci: fix cypress on node v18

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     video: false,
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://127.0.0.1:4173',
     supportFile: './tests/e2e/support/e2e.ts',
     specPattern: 'tests/e2e/**/*.spec.{ts,js}',
     setupNodeEvents(on, config) {
