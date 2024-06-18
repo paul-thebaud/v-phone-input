337fc2156995d84622a3ed4ab4f6b4dbec44bec5
paul <paul.thebaud29@gmail.com>
2024-06-18 18:14:00 +0200

    chore: upgrade ci actions and ignore dev code style issue

--- dev/plugins/vPhoneInput.ts
+++ dev/plugins/vPhoneInput.ts
@@ -3,6 +3,7 @@ import '@/scss/v-phone-input.scss';
 import 'flag-icons/css/flag-icons.min.css';
 import 'world-flags-sprite/stylesheets/flags32.css';
 
+// eslint-disable-next-line no-underscore-dangle
 declare const __E2E_TEST__: boolean | undefined;
 
 export default createVPhoneInput({
