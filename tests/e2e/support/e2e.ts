b7df8844246e410ff60119e9c4d4d7a75afca9a6
paul <paul.thebaud29@gmail.com>
2024-06-18 17:50:20 +0200

    chore: ignore resize observer error in e2e tests

--- tests/e2e/support/e2e.ts
+++ tests/e2e/support/e2e.ts
@@ -1,3 +1,10 @@
 import '@cypress/code-coverage/support';
 
 import './commands';
+
+// @ts-ignore
+Cypress.on('uncaught:exception', (err) => {
+  if (err.message.includes('ResizeObserver')) {
+    return false;
+  }
+});
